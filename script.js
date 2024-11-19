a0794498c8b8c877f9acae38fd4b1302c027a83a
OgAeons <opaeons@gmail.com>
2024-11-19 18:07:14 +0530

    Used axios to fetch data from the form

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 import "bootstrap"
 import "bootstrap/dist/css/bootstrap.min.css"
+import axios from "axios"
 
 const queryParamsContainer = document.querySelector('[data-query-params]')
 const requestHeadersContainer = document.querySelector('[data-request-headers]')
@@ -16,10 +17,33 @@ document.querySelector('[data-add-request-header-btn]').addEventListener('click'
 queryParamsContainer.append(createKeyValuePair())
 requestHeadersContainer.append(createKeyValuePair())
 
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    axios({
+        url: document.querySelector('[data-url]').value,
+        method: document.querySelector('[data-method]').value,
+        params: keyValuePairToObjects(queryParamsContainer),
+        headers: keyValuePairToObjects(requestHeadersContainer)
+    }).then(response => {
+        console.log(response)
+    })
+})
+
 function createKeyValuePair() {
     const element = keyValueTemplate.content.cloneNode(true)
     element.querySelector('[data-remove-btn]').addEventListener('click', (e) => {
         e.target.closest('[data-key-value-pair]').remove()
     })
     return element
+}
+
+function keyValuePairToObjects(container) {
+    const pairs = container.querySelectorAll('[data-key-value-pair]')
+    return [...pairs].reduce((data, pair) => {
+        const key = pair.querySelector('[data-key]').value
+        const value = pair.querySelector('[data-value]').value
+
+        if(key === '') return data
+        return {...data, [key]: value}
+    }, {})
 }
